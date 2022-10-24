50337eda528a4e78f098d7d4f5cba7fac3cda1b9
Diego Perales <dperales1313@gmail.com>
2022-10-24 19:09:45 -0400

    Se completa la interactividad usando JS del proyecto.

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ const backToProfileInfo = document.getElementById("back-button");
 shareMobileOptions.style.display = "none";
 
 function showShareMobileOptions() {
-    shareMobileOptions.style.display = 'flex';
-    profileInfo[0].style.display = 'none';
+    if(window.matchMedia("(min-width: 1440px)").matches) {
+        profileInfo[0].style.display = 'flex';
+        shareMobileOptions.style.display = 'flex';
+    } else {
+        shareMobileOptions.style.display = 'flex';
+        profileInfo[0].style.display = 'none';
+    }
 }
 
 function showProfileInfo() {
     profileInfo[0].style.display = 'flex';
     shareMobileOptions.style.display = 'none';
 }
-
-if (window.matchMedia('(min-width: 1440px)').matches) {
-     console.log('yes');
-     profileInfo[0].style.display = 'flex';
- }
 
 backToProfileInfo.addEventListener("click", showProfileInfo);
 shareButton.addEventListener("click", showShareMobileOptions);
